220321269bd5f688549f65c0bc005e273dffe378
Zhang <vincentzhang@ea.com>
2018-02-26 09:34:10 +0800

    Compile pass, continue debugging

--- app/src/main/java/com/max3d/primitives/Plane.java
+++ app/src/main/java/com/max3d/primitives/Plane.java
@@ -28,9 +28,9 @@ public class Plane extends ObjectRenderer {
         float[] color = {1.0f, 1.0f, 0.0f};
         float[] normal = {0.0f, 0.0f, 1.0f};
 
-        for (int row = 0; row < segsPerEdge; row++) {
-            for (int col = 0; col < segsPerEdge; col++) {
-                int startIdx = (row * segsPerEdge + col) * 3;
+        for (int row = 0; row <= segsPerEdge; row++) {
+            for (int col = 0; col <= segsPerEdge; col++) {
+                int startIdx = (row * (segsPerEdge + 1) + col) * 3;
                 positions[startIdx] = -width / 2 + col * segWidth;
                 positions[startIdx + 1] = -height / 2 + row * segHeight;
                 positions[startIdx + 2] = 0;
@@ -48,7 +48,7 @@ public class Plane extends ObjectRenderer {
         for (int row = 0; row < segsPerEdge; row++) {
             for (int col = 0; col < segsPerEdge; col++) {
                 int faceIdx = row * col;
-                faces[faceIdx] = row * segsPerEdge + col;
+d                faces[faceIdx] d= row * segsPerEdge + col;
                 faces[faceIdx + 1] = row * segsPerEdge + col + 1;
                 faces[faceIdx + 2] = (row + 1) * segsPerEdge + col + 1;
 
